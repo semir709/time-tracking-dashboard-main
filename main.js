8b537ea4e5adeee27fbc620a72736980bec84894
Semir Selman <selmansemir555@gmail.com>
2022-08-25 16:56:23 +0200

    made loading the json data

--- main.js
+++ main.js
@@ -32,3 +32,70 @@ btn_dots.forEach(el => {
     });
     
 });
+
+const role = document.querySelectorAll('.role p');
+const hours = document.querySelectorAll('.role-info h2');
+const last_week = document.querySelectorAll('.role-info p');
+
+selected = 'weekly';
+
+function getData() {
+
+    fetch("./data.json")
+    .then(response => {
+    return response.json();
+    })
+    .then(jsondata => {
+
+        for(let i = 0; i < jsondata.length; i++) {
+            role[i].innerHTML = jsondata[i].title;
+
+            if(selected == 'weekly') {
+
+                hours[i].innerHTML = jsondata[i].timeframes.weekly.current + 'hrs';
+                last_week[i].innerHTML = 'Last Week - ' + jsondata[i].timeframes.weekly.previous + 'hrs';
+
+            } else if(selected == 'daily') {
+
+                hours[i].innerHTML = jsondata[i].timeframes.daily.current + 'hrs';
+                last_week[i].innerHTML = 'Last Week - ' + jsondata[i].timeframes.daily.previous + 'hrs';
+
+            } else if (selected == 'monthly') {
+
+                hours[i].innerHTML = jsondata[i].timeframes.monthly.current + 'hrs';
+                last_week[i].innerHTML = 'Last Week - ' + jsondata[i].timeframes.monthly.previous + 'hrs';
+
+            } else {
+
+                role[i].innerHTML = 'nodata';
+                hours[i].innerHTML = 'nodata';
+                last_week[i].innerHTML = 'nodata';
+
+            }
+            
+        }
+        
+    
+    });
+
+}
+
+
+const list = document.querySelectorAll('.options ul li');
+
+list.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+        list.forEach(el => {el.classList.remove('active')});
+
+        e.target.classList.add('active');
+
+        selected = e.target.innerHTML.toLowerCase();
+
+        getData();
+
+    });
+
+});
+
